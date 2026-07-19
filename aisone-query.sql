0a33696ff6dd637b88bc3e19ad06d56f85f7000f
CNC Electric <cncelectric012@gmail.com>
2026-07-19 20:40:37 +0500

    fix: calculate outstanding from actual transactions (TBU_AccountAging is empty)

--- aisone-query.sql
+++ aisone-query.sql
@@ -2,10 +2,16 @@ SELECT
     CAST(a.PKGUID AS VARCHAR(50)) AS ErpID,
     a.CompanyName,
     ISNULL(a.Mobile1, ISNULL(a.Mobile2, ISNULL(a.Telephone1, a.Telephone2))) AS Phone,
-    ISNULL(CAST(SUM(ISNULL(ag.CDebit,0)) - SUM(ISNULL(ag.CCredit,0)) AS DECIMAL(18,2)), 0) AS Outstanding,
+    ISNULL(
+        (SELECT ISNULL(SUM(si.CGrandTotal), 0) FROM TBU_SaleInvoice si WHERE si.AccountDID = a.PKGUID AND si.VStatus <> 2)
+        -
+        (SELECT ISNULL(SUM(crv.CAmount), 0) FROM TBU_CashReceiveVoucher crv WHERE crv.AccountDID = a.PKGUID AND crv.VStatus <> 2)
+        -
+        (SELECT ISNULL(SUM(bdv.CAmount), 0) FROM TBU_BankDepositVoucher bdv WHERE bdv.AccountDID = a.PKGUID AND bdv.VStatus <> 2)
+        -
+        (SELECT ISNULL(SUM(mrvd.CAmount), 0) FROM TBU_MultipleReceivingVouchersDetail mrvd WHERE mrvd.AccountDID = a.PKGUID AND mrvd.VStatus <> 2)
+    , 0) AS Outstanding,
     ISNULL(a.CreditLimit, 0) AS CreditLimit
 FROM TBU_Accounts a
-LEFT JOIN TBU_AccountAging ag ON ag.AccountDID = a.PKGUID
 WHERE a.CompanyName IS NOT NULL AND LEN(LTRIM(RTRIM(a.CompanyName))) > 0
-GROUP BY a.PKGUID, a.CompanyName, a.Mobile1, a.Mobile2, a.Telephone1, a.Telephone2, a.CreditLimit
 ORDER BY a.CompanyName
